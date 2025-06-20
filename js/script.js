caf2d10a810d80fffca768bea96a11cd1646c360
codexmish <majharulislam.shanto.70@gmail.com>
2025-06-20 20:26:15 +0600

    Desktop page almost done

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
+
+// --------------slick slider Plugin........
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 4,
+    asNavFor: '.slider-for',
+    centerMode: true,
+    focusOnSelect: true,
+});
+
+// ----------------Testimonial slider
+
+// $('.testimonials-slider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+// });
